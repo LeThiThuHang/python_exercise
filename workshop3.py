7ae90e7ea9cef8cbfc6870146320d53bbdb3819f
LeThiThuHang <thuhang.lit@gmail.com>
2019-11-18 16:53:09 +0800

    first and secodn built in function

---
+++ workshop3.py
@@ -0,0 +1,39 @@
+""" pseudo code:
+    loop through every letter in the string
+    check if the letter is equal to char
+    if equal then return True
+    after the for loop, if none of the letter resemble the char, return False
+"""
+
+def is_in(char,string):
+    for i in range(len(string)):
+        if (char == string[i]):
+            print('True')
+            return True
+    
+    print('False')
+    return False
+    
+is_in("a","apple")
+is_in("a","bullshit")
+
+
+""" pseudo code:
+    loop through every element in the seq
+    check if the next element is larger than the previous element 
+    if not, then return False
+
+    after the for loop, if the whole seq has the next elements always larger than previous element , return True
+"""
+
+def is_sorted(seq):
+    for i in range(len(seq)):
+        if (seq[i] > seq[i+1]):
+            print('False')
+            return False
+    
+    print('True')
+    return True
+
+
+
